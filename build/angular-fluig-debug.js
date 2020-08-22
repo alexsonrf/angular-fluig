047e24382b8aa6714c6a112b531872f6c91e705f
Alex Ferreira <alexson_ferreira@hotmail.com>
2020-08-22 17:46:51 -0300

    commit

--- build/angular-fluig-debug.js
+++ build/angular-fluig-debug.js
@@ -1,7 +1,7 @@
 /**
  * angular-fluig
  * A list of AngularJS services, directives, filters, utilities an resources for Fluig
- * @version v1.0.5
+ * @version v1.0.3
  * @link 
  * @license MIT
  */
